41a06e7a37418888835ec629dafb6d9d6dff6f18
Kirill <smakvlad@gmail.com>
2021-01-15 15:08:13 +0200

    up

--- project/myapp/templatetags/random_slug.py
+++ project/myapp/templatetags/random_slug.py
@@ -5,5 +5,5 @@ from django import template
 register = template.Library()
 
 @register.filter
-def random_slugs(value):
+def random_numbers_slugs(value):
 	return '{}/{}/{}'.format(value, random.randint(1,100), ''.join(random.choices(string.ascii_letters, k=random.randint(5,10))))
