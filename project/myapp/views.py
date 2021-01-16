ba5f0d64555b8782041ee7060ba70911059861d0
Kirill <smakvlad@gmail.com>
2021-01-16 10:44:17 +0200

    pep

--- project/myapp/views.py
+++ project/myapp/views.py
@@ -4,20 +4,26 @@ import string
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
 	return render(request, 'index.html')
+
 
 def articles(request):
 	return render(request, 'articles.html')
 
+
 def articles_arhive(request):
 	return render(request, 'articles_arhive.html')
+
 
 def users(request):
 	return render(request, 'users.html')
 
+
 def article(request):
 	return HttpResponse('article')
+
 
 def article_num(request, article_number):
 	data = {
@@ -25,8 +31,10 @@ def article_num(request, article_number):
 	}
 	return render(request, 'num.html', data)
 
+
 def article_num_arhive(request, article_number):
 	return HttpResponse('{} arhive'.format(article_number))
+
 
 def article_num_slug(request, article_number, slug_text):
 	data = {
@@ -35,11 +43,14 @@ def article_num_slug(request, article_number, slug_text):
 	}
 	return render(request, 'num.html', data)
 
+
 def users_num(request, user_number):
 	return HttpResponse('{} - user'.format(user_number))
 
+
 def phone(request):
 	return HttpResponse('phone')
+
 
 def symbol(request):
 	return HttpResponse('symbol')
