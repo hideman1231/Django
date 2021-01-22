131ca861fbc7f2f1fbd2d514e6311da68e140755
Kirill <smakvlad@gmail.com>
2021-01-22 21:08:21 +0200

    base forms

--- project_models/myapp/views.py
+++ project_models/myapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Article, Comment
+from .forms import ArticleForm
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+
+def index(request):
+	form = Article.objects.all()
+	return render(request, 'index.html', {'form': form})
+
+def create(request):
+	if request.method == 'POST':
+		form = ArticleForm(request.POST)
+		if form.is_valid():
+			return HttpResponseRedirect('/')
+	else:
+		form = ArticleForm()
+	print(form.is_valid())
+	return render(request, 'create.html', {'form': form})
