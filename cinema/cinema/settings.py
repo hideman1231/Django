48770317788c0f8eb46729410188c2af50767342
Kirill <smakvlad@gmail.com>
2021-03-31 20:28:39 +0300

    set

--- cinema/cinema/settings.py
+++ cinema/cinema/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'cinema.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates',],
+        'DIRS': ['templates', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
